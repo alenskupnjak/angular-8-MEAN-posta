b11715ac4321bba41b380acc337dd71301bcc0e4
AlenS <alenskupnjak@yahoo.com>
2020-09-11 10:12:46 +0200

    041 error dialog

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -29,6 +29,9 @@ import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { AuthInterceptor } from "./auth/auth-interceptor";
+import { ErrorInterceptor } from "./error-interceptor";
+import { MatDialogModule } from "@angular/material/dialog";
+import { ErrorComponent } from "./error/error.component";
 
 @NgModule({
   declarations: [
@@ -39,6 +42,7 @@ import { AuthInterceptor } from "./auth/auth-interceptor";
     PostListComponent,
     LoginComponent,
     SignupComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,10 +58,16 @@ import { AuthInterceptor } from "./auth/auth-interceptor";
     MatProgressSpinnerModule, // material
     ReactiveFormsModule, // REACTIVE FORMS
     MatPaginatorModule, // material, paginacija
+    MatDialogModule, // dialog on ekranu
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
+
+  // entryComponents - se korist samo kada Ang-component nema selector i ne pristupa
+  // se kroz router. Angular ne zna da ova komponenta postoji jer se kreira se u hodu (npr. dialog)
+  entryComponents: [ErrorComponent],
 })
 export class AppModule {}
